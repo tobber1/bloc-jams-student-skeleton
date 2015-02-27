373280ed7e9e97dfaa0ee6beec2cece1d68e91b2
Todd Caschette <laugh_aloud80026@yahoo.com>
2015-02-27 23:26:41 +0000

    Added jQuery animations to landing page on BJsNew

---
+++ app/scripts/landing.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+  
+  $('.hero-content h3').click(function() {
+    var subText = $(this).text();
+    $(this).text(subText + "!");
+  });
+  
+  var onHoverAction = function(event) {
+    console.log("On-hover action triggered");
+    $(this).animate({'margin-top': '10px'});
+  };
+  
+  var offHoverAction = function(event) {
+    console.log("Off-hover action triggered");
+    $(this).animate({'margin-top': '0px'});
+  };
+  
+  $('.selling-points .point').hover(onHoverAction, offHoverAction);
+  
+});
